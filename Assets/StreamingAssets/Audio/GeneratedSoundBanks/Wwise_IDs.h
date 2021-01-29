7f8b07dc1f9501cf5f8089adb64114f1850236b6
Noizion <noizionsounddesign@gmail.com>
2021-01-30 00:17:51 +0300

    hgj
    jhk

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,104 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRDS = 352130103U;
+        static const AkUniqueID CARS_1 = 1774615368U;
+        static const AkUniqueID CARS_2 = 1774615371U;
+        static const AkUniqueID DIALOG = 1235749641U;
+        static const AkUniqueID GARBAGE_1 = 3900621502U;
+        static const AkUniqueID GARBAGE_2 = 3900621501U;
+        static const AkUniqueID GARBAGE_3 = 3900621500U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_WINDOW_1 = 1690538590U;
+        static const AkUniqueID PLAY_WINDOW_2 = 1690538589U;
+        static const AkUniqueID PLAY_WINDOW_3 = 1690538588U;
+        static const AkUniqueID PLAY_WINDOW_4 = 1690538587U;
+        static const AkUniqueID RAIN_1 = 965942311U;
+        static const AkUniqueID RAIN_2 = 965942308U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ONE
+        {
+            static const AkUniqueID GROUP = 1064933119U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FIVE = 2611770117U;
+                static const AkUniqueID FOUR = 2863728729U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID ONE = 1064933119U;
+                static const AkUniqueID THREE = 912956111U;
+                static const AkUniqueID TWO = 678209053U;
+            } // namespace STATE
+        } // namespace ONE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FLOOR = 1088209313U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ONE = 1064933119U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID ONE = 1064933119U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SPATIAL_SOUNDS = 3287411158U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID EXT = 898834102U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ROOM = 2077253480U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
